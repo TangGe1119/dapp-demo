dd367c17cff4d459e3c2a5ac9763ff2582f35481
Toby Yuan <1055410108@qq.com>
2018-05-29 15:17:55 +0800

    init project

---
+++ components/Header.js
@@ -0,0 +1,65 @@
+import React from 'react'
+import { AppBar, Toolbar, Typography, Button } from '@material-ui/core'
+import { withStyles } from '@material-ui/core/styles'
+
+const styles = {
+    wrapper: {
+        margin: '0 auto',
+        width: '80%',
+        maxWidth: '1200px',
+        display: 'flex'
+    },
+    brand: {
+        borderRight: '2px solid #CCCCCC',
+        paddingRight: '1em',
+        marginRight: '1em'
+    },
+    toolbar: {
+        padding: 0,
+        flex: 1
+    },
+    flexContainer: {
+        flex: 1
+    },
+    anchor: {
+        textDecoration: 'none'
+    }
+}
+
+class Header extends React.Component {
+    render() {
+        const { classes } = this.props
+
+        return (
+            <AppBar position="static" color="default">
+                <div className={classes.wrapper}>
+                    <Toolbar className={classes.toolbar}>
+                        <Typography
+                            variant="title"
+                            color="inherit"
+                            className={classes.brand}
+                        >
+                            众筹 DApp
+                        </Typography>
+
+                        <Typography
+                            className={classes.flexContainer}
+                            variant="title"
+                            color="inherit"
+                        >
+                            <a href="/" className={classes.anchor}>
+                                项目列表
+                            </a>
+                        </Typography>
+
+                        <Button variant="raised" color="primary">
+                            发起项目
+                        </Button>
+                    </Toolbar>
+                </div>
+            </AppBar>
+        )
+    }
+}
+
+export default withStyles(styles)(Header)
